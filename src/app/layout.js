d53e44a5522fda0cd321a43d049d7053e50af42f
Wout Demeyere <wout@unit6.be>
2023-10-02 11:47:12 +0200

    added favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,8 +5,16 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Spacemakers',
-  description: 'Spacemakers: Extending reality, pixel by ixel',
+  description: 'Spacemakers: Extending reality, pixel by pixel',
+  icons: {
+    appleTouchIcon: '/apple-touch-icon.png',
+    favicon32: '/favicon-32x32.png',
+    favicon16: '/favicon-16x16.png',
+    // manifest: '/site.webmanifest',
+    // maskIcon: '/safari-pinned-tab.svg',
+  }
 }
+
 
 export default function RootLayout({ children }) {
   return (
