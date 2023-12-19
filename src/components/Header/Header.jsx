3aae77ac50c57888f882bb5d7a1b78ca1b93c606
Wout Demeyere <wout@unit6.be>
2023-12-19 14:59:12 +0100

    Header fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 
+import {useRouter} from "next/router";
+
 import styles from './Header.module.css'
 import NavBar from "./NavBar";
 
 const Header = () => {
+    const router = useRouter();
+
+    const goHome = () => {
+        router.push('/');
+    }
 
     return (
         <header className={styles.header}>
-            <img className={styles.header__logo} src="/images/logo_no_text2.png" alt="logo" />
+            <img    className={styles.header__logo} 
+                    src="/images/logo_no_text2.png" 
+                    alt="logo" 
+                    onClick={goHome}
+                    />
             <NavBar />
         </header>
     );
